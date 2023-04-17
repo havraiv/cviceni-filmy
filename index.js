914d701a02c14baeeb684f71d9e12b5bd4a25a4f
Iva Havranová <havranova.i@seznam.cz>
2023-04-17 17:27:37 +0200

    První část úkolu done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 console.log('funguju!');
+
+fetch('https://apps.kodim.cz/daweb/trening-api/apis/movie-api/movies')
+  .then((response) => response.json())
+
+  .then((films) => {
+    console.log(films);
+
+    films.forEach((film) => {
+      document.querySelector(
+        '.movie-list',
+      ).innerHTML += `<li class="movie-detail">
+        <div class="movie-poster">
+          <img
+            src="${film.posterUrl}"
+            alt="${film.title}"
+          />
+        </div>
+        <div class="movie-info">
+          <h2 class="movie-title">${film.title}</h2>
+          <div class="movie-year">Rok vydání:${film.year}</div>
+          <div class="movie-link">
+            <a href="${film.url}" target="_blank">Odkaz na CSFD</a>
+          </div>
+        </div>
+        </li>`;
+    });
+  });
